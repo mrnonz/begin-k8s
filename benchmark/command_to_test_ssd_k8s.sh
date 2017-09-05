ed1bc83c656d5e389fa5bb018cc97fa7eace7542
Nontawat Numor <nontawat39@gmail.com>
2017-09-05 18:21:04 +0700

    Save command to test ssd :tea:

---
+++ benchmark/command_to_test_ssd_k8s.sh
@@ -0,0 +1,23 @@
+# Random write test for IOP/s, i.e. lots of small files
+sync;fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --directory=/test-ssd --filename=/test-ssd --bs=4k --iodepth=256 --size=4G --readwrite=randwrite --ramp_time=4
+
+# Random Read test for IOP/s, i.e. lots of small files
+sync;fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --directory=/test-ssd --filename=/test-ssd --bs=4k --iodepth=256 --size=4G --readwrite=randread --ramp_time=4
+
+# Sequential write test for IOP/s, i.e. one large file
+sync;fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --directory=/test-ssd --filename=/test-ssd --bs=4k --iodepth=256 --size=4G --readwrite=write --ramp_time=4
+
+# Sequential Read test for IOP/s, i.e. one large file
+sync;fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --directory=/test-ssd --filename=/test-ssd --bs=4k --iodepth=256 --size=4G --readwrite=read --ramp_time=4
+
+# Random write test for throughput, i.e. lots of small files
+sync;fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --directory=/test-ssd --filename=/test-ssd --bs=4M --iodepth=256 --size=10G --readwrite=randwrite --ramp_time=4
+
+# Random Read test for throughput
+sync;fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --directory=/test-ssd --filename=/test-ssd --bs=4M --iodepth=256 --size=10G --readwrite=randread --ramp_time=4
+
+# Sequential write test for throughput, i.e. one large file
+sync;fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --directory=/test-ssd --filename=/test-ssd --bs=4M --iodepth=256 --size=10G --readwrite=write --ramp_time=4
+
+# Sequential Read test for throughput, i.e. one large file
+sync;fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --directory=/test-ssd --filename=/test-ssd --bs=4M --iodepth=256 --size=10G --readwrite=read --ramp_time=4
